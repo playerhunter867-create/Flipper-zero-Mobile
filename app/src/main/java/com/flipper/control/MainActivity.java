3f41fc7e35bfa707a74babd8add96cf08dbb1fa3
playerhunter867-create <playerhunter867@gmail.com>
2026-08-13 13:55:48 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/flipper/control/MainActivity.java
@@ -0,0 +1,16 @@
+package com.flipper.control;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.tvOutput);
+        tv.setText("> FLIPPER ZERO\n> IR READY\n> SELECT SCAN");
+    }
+}
