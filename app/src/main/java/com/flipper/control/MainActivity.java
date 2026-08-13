eaa3c920aca3e0534ddedba82a54c7f4ac4a168f
playerhunter867-create <playerhunter867@gmail.com>
2026-08-13 16:25:26 +0300

    Update MainActivity.java

--- app/src/main/java/com/flipper/control/MainActivity.java
+++ app/src/main/java/com/flipper/control/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView tv = findViewById(R.id.tvOutput);
-        tv.setText("FLIPPER ZERO\nРАБОТАЕТ!");
+        tv.setText("РАБОТАЕТ!");
     }
 }
